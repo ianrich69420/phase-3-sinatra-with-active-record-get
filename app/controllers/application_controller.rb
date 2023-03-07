4480f21aea5dd0046a6b7797ef2fecd92ca59c16
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-03-07 09:28:47 +0300

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
+  get '/', :provides => :json do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games', :provides => :json do
+    # get all the games from the database
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id', :provides => :json do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(include: { reviews: { except: [:user_id, :game_id], include: :user } })
   end
 
 end
